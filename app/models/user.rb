f09978326a9db1188c60b7a6a2c10b668cfd4931
Pradyumna <pradyumna2905@gmail.com>
2018-07-29 12:38:03 -0700

    Fix col name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  def ynab_connected?
+    self.ynab_access_token.present?
+  end
 end
--- db/migrate/20180729191033_add_ynab_access_token_to_users.rb
+++ db/migrate/20180729191033_add_ynab_access_token_to_users.rb
@@ -1,5 +1,5 @@
 class AddYnabAccessTokenToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :ynab_acces_token, :string
+    add_column :users, :ynab_access_token, :string
   end
 end
