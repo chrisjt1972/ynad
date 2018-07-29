86441d5aad1e2e79d8c93bd01521f3747fe0d23b
Pradyumna <pradyumna2905@gmail.com>
2018-07-29 10:47:21 -0700

    Dashboard placeholder.

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
     unauthenticated :user do
       root to: 'devise/registrations#new'
     end
+
+    authenticated :user do
+      root to: 'dashboards#show', as: :authenticated_root
+    end
   end
 end
