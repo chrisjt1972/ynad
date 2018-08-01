9c275e7752c49e6383ea5943158429315548abb4
Pradyumna <pradyumna2905@gmail.com>
2018-07-31 17:33:34 -0700

    Setup sidekiq.

--- Gemfile
+++ Gemfile
@@ -54,6 +54,9 @@ gem 'httparty'
 # Application settings
 gem 'settingslogic'
 
+# Background job
+gem 'sidekiq'
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Yanab
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.active_job.queue_adapter = :sidekiq
 
     require_dependency Rails.root.join('lib/oauth/ynab/client')
 
