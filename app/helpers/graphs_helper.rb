34c0a6ef18897247f5279a2d22d7c7752a0c22b8
Pradyumna <pradyumna2905@gmail.com>
2018-08-06 12:19:53 -0700

    Graph tweaks.

--- app/helpers/graphs_helper.rb
+++ app/helpers/graphs_helper.rb
@@ -2,11 +2,18 @@ module GraphsHelper
   def cashflow_graph
     data = {}
 
-    @transactions.select(&:expense?).each do |transaction|
-      data[transaction.date.strftime('%d, %B %Y')] = transaction.amount.abs / 1000
+    @transactions.each do |transaction|
+      data[transaction.date.strftime('%d, %B %Y')] = transaction.amount / 1000
     end
 
-    area_chart(data)
+    area_chart(
+      data,
+      label: 'Amount',
+      xtitle: 'Last 90 days',
+      points: false,
+      colors: ['#1E8495'],
+      prefix: '$'
+    )
   end
 
   def bank_balances
@@ -16,6 +23,11 @@ module GraphsHelper
       data[account.name] = account.balance / 1000
     end
 
-    bar_chart data
+    bar_chart(
+      data,
+      label: 'Amount',
+      colors: ['#1E8495'],
+      prefix: '$'
+    )
   end
 end
