a18a34053257823f375ccaac8fa908c67c0d6061
Pradyumna <pradyumna2905@gmail.com>
2018-08-07 16:56:13 -0700

    Change dashboards_controller.

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -5,8 +5,11 @@ class DashboardsController < ApplicationController
              else
                current_user&.budgets&.first
              end
-    @transactions = budget.accounts.map(&:transactions).flatten.select(&:within_90_days?) if budget
-    @accounts = budget.accounts if budget
+
+    if budget
+      @transactions = budget.accounts.map(&:transactions).flatten.select(&:within_90_days?)
+      @accounts = budget.accounts
+    end
   end
 
   private
