246f2b6a8976b83d000470e200b3d67f339cf26e
Pradyumna <pradyumna2905@gmail.com>
2018-08-08 11:03:01 -0700

    Adds chart labels.
    - Return category groups without YNAB internal categories.

--- app/models/category_group.rb
+++ app/models/category_group.rb
@@ -4,4 +4,14 @@ class CategoryGroup < ApplicationRecord
 
   validates_uniqueness_of :ynab_id
   validates_presence_of :name, :ynab_id
+
+  YNAB_INTERNAL_CATEGORIES = [
+    'Internal Master Category'
+  ].freeze
+
+  scope :without_ynab_internal_categories, -> {
+    reject { |category_group| YNAB_INTERNAL_CATEGORIES.include?(
+      category_group.name
+    ) }
+  }
 end
