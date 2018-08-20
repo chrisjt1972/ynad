cd6e0dd01c1e89c6b1032bdc99603d35e81fafa3
Pradyumna <pradyumna2905@gmail.com>
2018-08-19 18:17:06 -0700

    Change email for mock.

--- db/fixtures/development/00_users.rb
+++ db/fixtures/development/00_users.rb
@@ -2,7 +2,7 @@
   User.seed do |s|
     s.first_name = name
     s.last_name = 'Doe'
-    s.email = "#{name.downcase}@example.com"
+    s.email = "#{name.downcase}@ynad.com"
     s.password = 'password123'
     s.password_confirmation = 'password123'
     s.ynab_access_token = SecureRandom.base64
--- db/fixtures/development/01_budgets.rb
+++ db/fixtures/development/01_budgets.rb
@@ -2,7 +2,7 @@ User.where(email: ['john@example.com', 'mary@example.com']).each do |user|
   2.times do |index|
     Budget.seed do |s|
       s.user_id = user.id
-      s.name = "#{user.first_name}'s Budgeti (#{index})"
+      s.name = "#{user.first_name}'s Budget (#{index})"
       s.ynab_id = SecureRandom.uuid
     end
   end
