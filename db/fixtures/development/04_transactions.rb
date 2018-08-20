d8812bd49f60ce4ead308a9f6370ec23c4cb0325
Pradyumna <pradyumna2905@gmail.com>
2018-08-19 17:13:33 -0700

    Reasonable data.

--- db/fixtures/development/04_transactions.rb
+++ db/fixtures/development/04_transactions.rb
@@ -1,7 +1,7 @@
 User.where(email: ['john@example.com', 'mary@example.com']).each do |user|
   user.accounts.each do |account|
     # Expenses
-    50.times do
+    10.times do
       account.transactions.create!(
         date: Faker::Date.backward(90),
         amount: (-1000*Random.rand(100..500)),
@@ -11,7 +11,7 @@ User.where(email: ['john@example.com', 'mary@example.com']).each do |user|
     end
 
     # Income
-    10.times do
+    5.times do
       account.transactions.create!(
         date: Faker::Date.backward(90),
         amount: Random.rand(1000..5000) * 1000,
